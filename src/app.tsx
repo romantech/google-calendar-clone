f82ce24599d1342577afae6c6028667e6b3f2393
ColorFilter <johan@romantech.net>
2025-05-17 18:09:55 +0900

    redux-toolkit 패키지 추가 및 기본 설정

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,23 @@
+import { useAppDispatch, useAppSelector } from './store/hooks';
+import { increment } from './store/counter-slice';
+
 function App() {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
   return (
     <main>
       <h1 className="underline">Hello world!</h1>
+      <div>{`count: ${count}`}</div>
+      <button
+        type="button"
+        className="cursor-pointer"
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
     </main>
   );
 }
