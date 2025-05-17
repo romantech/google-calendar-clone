a688a9205e60fcad6ace9b47d4adbc8b143778bb
ColorFilter <johan@romantech.net>
2025-05-17 16:29:22 +0900

    ESLint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,22 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettierPluginRecommended from 'eslint-plugin-prettier/recommended';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  prettierPluginRecommended,
+
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -19,10 +24,16 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'prettier/prettier': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // 타입 import 할 때 인라인으로 type 키워드 추가 e.g., import { type Circle } from '...'
+      '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
     },
   },
-)
+  {
+    /** @see https://typescript-eslint.io/packages/typescript-eslint/#flat-config-extends */
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+);
