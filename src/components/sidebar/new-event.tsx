ff14d4094413698e0f73c015d5d3bdef95d89111
ColorFilter <johan@romantech.net>
2025-05-18 18:15:42 +0900

    만들기 버튼 추가

---
+++ src/components/sidebar/new-event.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components/ui/button';
+import { ChevronDown, Plus } from 'lucide-react';
+import { cn } from '@/lib';
+
+interface NewEventProps {
+  className?: string;
+}
+
+export default function NewEvent({ className }: NewEventProps) {
+  return (
+    <Button
+      variant="ghost"
+      className={cn(
+        'cursor-not-allowed rounded-lg border border-gray-700 p-6 hover:bg-slate-200',
+        className,
+      )}
+    >
+      <Plus />
+      <span>만들기</span>
+      <ChevronDown />
+    </Button>
+  );
+}
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,8 +1,10 @@
-import DatePicker from '@/components/sidebar/date-picker';
+import DatePicker from './date-picker';
+import NewEvent from './new-event';
 
 export default function Sidebar() {
   return (
     <aside className="w-full max-w-64 p-4">
+      <NewEvent className="mb-4" />
       <DatePicker />
     </aside>
   );
